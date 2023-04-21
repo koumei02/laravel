40610621805065448fa65206e83865415548c8d6
koumei02 <koumei.h.nca@outlook.jp>
2023-04-21 23:29:51 +0900

    ログイン画面

---
+++ app/app/Http/Controllers/AutoController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //データの一覧表示ログイン後の画面系統に関して
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //登録用のフォーム。フォームを表示させる。食事登録、体重登録に関して。
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //登録作業を書く箇所。
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //詳細画面の表示。
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //編集のフォームを表示させる。プロフィール画面
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //編集処理を行う。保存について
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //削除処理。投稿内容の削除。
+    }
+}
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,4 +1,8 @@
 <?php
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AutoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -10,7 +14,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('auto','AutoController');
 });
+
+
+
